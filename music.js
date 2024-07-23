255984f2d341f8100e552857b1ce9328dcbf2764
tanya1725 <tanyagupta172510@gmail.com>
2024-07-23 16:32:00 +0530

    Initial commit

---
+++ music.js
@@ -0,0 +1,329 @@
+arjit = [
+  "Tum se he",
+  "Mithi Mithi Baat",
+  "Kallo",
+  "Chandigarh Ka Chokra",
+  "Tu Har Lamha",
+  "husn",
+  "Ve Haaniya",
+  "Bool halke halke",
+  "Zara sa",
+  "Tere Hawaale",
+  "Pehle Bhi Main",
+  "Mehrama",
+  "Saware",
+  "Zaalima",
+  "Tu Jaane Na",
+  "Kali Kali Zulfon Ke",
+  "Billo Rani",
+  "CHUDI PAYAL",
+  "Tera Zikr",
+  "Hey Shona",
+];
+
+topromance = [
+  "Tum Hi Ho",
+  "Raabta,Janam Janam",
+  "Tum Mile",
+  "Sun Saathiya",
+  "Mast Magan",
+  "Jeene Laga Hoon",
+  "Hasi Ban Gaye",
+  "Gerua",
+  "Kaise Hua",
+  "Tum Jo Aaye",
+  "Enna Sona",
+  "Main Rang Sharbaton Ka",
+  "Pehli Dafa",
+  "Zaalima",
+  "Tu Jaane Na",
+  "Kali Kali Zulfon Ke",
+  "Billo Rani",
+  "CHUDI PAYAL",
+  "Tera Zikr",
+  "Hey Shona",
+];
+topromanceduration = [
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:16",
+  "2:27",
+  "6:04",
+  "3:09",
+  "6:04",
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:09",
+  "3:16",
+  "2:27",
+];
+duration = [
+  "6:04",
+  "3:09",
+  "6:04",
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:09",
+  "3:16",
+  "2:27",
+  "3:16",
+  "2:27",
+];
+topwek = [
+  "Pehle Bhi Main",
+  "Mehrama",
+  "Saware",
+  "Zaalima",
+  "Tu Jaane Na",
+  "Kali Kali Zulfon Ke",
+  "Billo Rani",
+  "CHUDI PAYAL",
+  "Tera Zikr",
+  "Hey Shona",
+  "Tum Hi Ho",
+  "Raabta,Janam Janam",
+  "Tum Mile",
+  "Sun Saathiya",
+  "Mast Magan",
+  "Jeene Laga Hoon",
+  "Hasi Ban Gaye",
+  "Gerua",
+  "Kaise Hua",
+  "Tum Jo Aaye",
+];
+k = 0;
+m = 0;
+s = 0;
+function menu() {
+  document.getElementById("menuicon").style.display = "block";
+  document.getElementById("mix").style.display = "none";
+  document.getElementById("cross").style.display = "block";
+}
+function cbtn() {
+  document.getElementById("menuicon").style.display = "none";
+  document.getElementById("cross").style.display = "none";
+  document.getElementById("mix").style.display = "block";
+}
+
+function play() {
+  document.getElementById("con").style.display = "none";
+  document.getElementById("navid").style.display = "none";
+  document.getElementById("con1").style.display = "none";
+  document.getElementById("con2").style.display = "none";
+  document.getElementById("con3").style.display = "none";
+  document.getElementById("con4").style.display = "none";
+  document.getElementById("con5").style.display = "none";
+  document.getElementById("con6").style.display = "none";
+  document.getElementById("back-again").style.display = "none";
+  // document.getElementById("login").style.display = "none";
+
+  document.getElementById("fotter").style.display = "none";
+  document.getElementById("side-bar").style.display = "block";
+  document.getElementById("logo-side").style.display = "block";
+
+  document.getElementById("lib").style.display = "block";
+  document.getElementById("exp").style.display = "block";
+  document.getElementById("txetecho").style.display = "block";
+  document.getElementById("mslist").style.display = "block";
+}
+function play1() {
+  document.getElementById("con").style.display = "none";
+  document.getElementById("navid").style.display = "none";
+  document.getElementById("con1").style.display = "none";
+  document.getElementById("con2").style.display = "none";
+  document.getElementById("con3").style.display = "none";
+  document.getElementById("con4").style.display = "none";
+  document.getElementById("con5").style.display = "none";
+  document.getElementById("con6").style.display = "none";
+  document.getElementById("back-again").style.display = "none";
+  // document.getElementById("login").style.display = "none";
+
+  document.getElementById("fotter").style.display = "none";
+  document.getElementById("side-bar").style.display = "block";
+  document.getElementById("logo-side").style.display = "block";
+
+  document.getElementById("lib").style.display = "block";
+  document.getElementById("exp").style.display = "block";
+  document.getElementById("txetecho").style.display = "block";
+  document.getElementById("mslist").style.display = "block";
+}
+
+function play2() {
+  document.getElementById("con").style.display = "none";
+  document.getElementById("navid").style.display = "none";
+  document.getElementById("con1").style.display = "none";
+  document.getElementById("con2").style.display = "none";
+  document.getElementById("con3").style.display = "none";
+  document.getElementById("con4").style.display = "none";
+  document.getElementById("con5").style.display = "none";
+  document.getElementById("con6").style.display = "none";
+  document.getElementById("back-again").style.display = "none";
+  // document.getElementById("login").style.display = "none";
+
+  document.getElementById("fotter").style.display = "none";
+  document.getElementById("side-bar").style.display = "block";
+  document.getElementById("logo-side").style.display = "block";
+
+  document.getElementById("lib").style.display = "block";
+  document.getElementById("exp").style.display = "block";
+  document.getElementById("txetecho").style.display = "block";
+  document.getElementById("mslist").style.display = "block";
+}
+function play3() {
+  document.getElementById("con").style.display = "none";
+  document.getElementById("navid").style.display = "none";
+  document.getElementById("con1").style.display = "none";
+  document.getElementById("con2").style.display = "none";
+  document.getElementById("con3").style.display = "none";
+  document.getElementById("con4").style.display = "none";
+  document.getElementById("con5").style.display = "none";
+  document.getElementById("con6").style.display = "none";
+  document.getElementById("back-again").style.display = "none";
+  document.getElementById("fotter").style.display = "none";
+
+  document.getElementById("side-bar").style.display = "block";
+  document.getElementById("logo-side").style.display = "block";
+  document.getElementById("lib").style.display = "block";
+  document.getElementById("exp").style.display = "block";
+  document.getElementById("txetecho").style.display = "block";
+  document.getElementById("mslist").style.display = "block";
+}
+function web() {
+  document.getElementById("mslist").style.display = "none";
+  document.getElementById("side-bar").style.display = "block";
+  document.getElementById("back-again").style.display = "block";
+  document.getElementById("ms-side").style.display = "block";
+  document.getElementById("main1").style.display = "block";
+  document.getElementById("exp").style.display = "none";
+  document.getElementById("rom1").style.display = "none";
+  document.getElementById("web1").style.display = "block";
+  document.getElementById("tbl1").style.display = "block";
+  document.getElementById("top-roma").style.display = "none";
+  document.getElementById("wekly").style.display = "none";
+  document.getElementById("top-weekly").style.display = "none";
+  for (i = 0; i < 23; i++) {
+    document.getElementById("t" + (i + 1)).innerHTML = arjit[i];
+
+    document.getElementById("dt" + (i + 1)).innerHTML = duration[i];
+  }
+}
+
+function audio(p) {
+  k = p;
+  document.getElementById("aud").src =
+    document.getElementById("t" + (p + 1)).innerHTML + ".mp3";
+  aud.play();
+}
+function romaudio(n) {
+  m = n;
+  document.getElementById("aud").src =
+    document.getElementById("r" + (n + 1)).innerHTML + ".mp3";
+  aud.play();
+}
+function weeklyt(d) {
+  s = d;
+  document.getElementById("aud").src =
+    document.getElementById("w" + (d + 1)).innerHTML + ".mp3";
+  aud.play();
+}
+function back() {
+  document.getElementById("con").style.display = "block";
+  document.getElementById("navid").style.display = "block";
+  document.getElementById("con1").style.display = "block";
+  document.getElementById("con2").style.display = "block";
+  document.getElementById("con3").style.display = "block";
+  document.getElementById("con4").style.display = "block";
+  document.getElementById("con5").style.display = "block";
+  document.getElementById("con6").style.display = "block";
+
+  document.getElementById("fotter").style.display = "block";
+  document.getElementById("side-bar").style.display = "none";
+  document.getElementById("logo-side").style.display = "none";
+  document.getElementById("lib").style.display = "none";
+  document.getElementById("exp").style.display = "none";
+  document.getElementById("txetecho").style.display = "none";
+  document.getElementById("mslist").style.display = "none";
+}
+function backagain() {
+  document.getElementById("mslist").style.display = "block";
+  document.getElementById("exp").style.display = "block";
+  document.getElementById("back-again").style.display = "none";
+  document.getElementById("ms-side").style.display = "none";
+}
+function romanc() {
+  document.getElementById("ms-side").style.display = "block";
+  document.getElementById("web1").style.display = "none";
+  document.getElementById("mslist").style.display = "none";
+  document.getElementById("side-bar").style.display = "block";
+  document.getElementById("back-again").style.display = "block";
+  document.getElementById("main1").style.display = "block";
+  document.getElementById("exp").style.display = "none";
+  document.getElementById("rom1").style.display = "block";
+  document.getElementById("tbl1").style.display = "none";
+  document.getElementById("top-roma").style.display = "block";
+  document.getElementById("wekly").style.display = "none";
+  document.getElementById("top-weekly").style.display = "none";
+  for (l = 0; l < 20; l++) {
+    document.getElementById("r" + (l + 1)).innerHTML = topromance[l];
+
+    document.getElementById("dr" + (l + 1)).innerHTML = topromanceduration[l];
+  }
+}
+function weekly() {
+  document.getElementById("top-roma").style.display = "none";
+  document.getElementById("ms-side").style.display = "block";
+  document.getElementById("wekly").style.display = "block";
+
+  document.getElementById("web1").style.display = "none";
+  document.getElementById("mslist").style.display = "none";
+  document.getElementById("side-bar").style.display = "block";
+  document.getElementById("back-again").style.display = "block";
+  document.getElementById("main1").style.display = "block";
+  document.getElementById("exp").style.display = "none";
+  document.getElementById("rom1").style.display = "none";
+  document.getElementById("tbl1").style.display = "none";
+  document.getElementById("top-roma").style.display = "none";
+  document.getElementById("top-weekly").style.display = "block";
+  for (h = 0; h < 20; h++) {
+    document.getElementById("w" + (h + 1)).innerHTML = topwek[h];
+
+    document.getElementById("dw" + (h + 1)).innerHTML = topromanceduration[h];
+  }
+}
+function heart() {
+  document.getElementById("mslist").style.display = "none";
+  document.getElementById("side-bar").style.display = "block";
+  document.getElementById("back-again").style.display = "block";
+  document.getElementById("ms-side").style.display = "block";
+  document.getElementById("main1").style.display = "block";
+  document.getElementById("exp").style.display = "none";
+  document.getElementById("rom1").style.display = "none";
+  document.getElementById("web1").style.display = "block";
+  document.getElementById("tbl1").style.display = "block";
+  document.getElementById("top-roma").style.display = "none";
+  document.getElementById("wekly").style.display = "none";
+  document.getElementById("top-weekly").style.display = "none";
+  for (i = 0; i < 23; i++) {
+    document.getElementById("t" + (i + 1)).innerHTML = arjit[i];
+
+    document.getElementById("dt" + (i + 1)).innerHTML = duration[i];
+  }
+}
